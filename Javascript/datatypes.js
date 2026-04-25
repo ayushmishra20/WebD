28ac81296ec3ed09a879dc0a8b13a918c3de0866
Ayush Mishra <officialayushmishra002@gmail.com>
2026-04-26 00:10:03 +0530

    js

---
+++ Javascript/datatypes.js
@@ -0,0 +1,18 @@
+let Name = "Ayush"
+let age = 19
+let accountID = 1234
+
+console.table([Name, age, accountID])
+
+// number => 2 to pwr 53
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = value not given to variable
+// symbol = used for unique value
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof age)
