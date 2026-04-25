c012666a69cb1e0a1f3ec8d05e93178e15746739
Ayush Mishra <officialayushmishra002@gmail.com>
2026-04-25 23:48:50 +0530

    learning js

---
+++ Javascript/besic.js
@@ -0,0 +1,17 @@
+const account = 123445
+let Name = "Ayush"
+var Age = 20
+ammount = 123
+let accountstate;
+
+// To see the one 
+console.log(ammount)
+
+// To see the all placeholer value in table form
+console.table([account, Name, ammount, Age, accountstate])
+
+/*This is
+ multiline 
+comment
+// Prefer not use to var , use let 
+*/
