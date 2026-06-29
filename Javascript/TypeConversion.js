41b85fb46c8c018cca81013cc0cebd3e1499d4e1
Ayush Mishra <officialayushmishra002@gmail.com>
2026-06-29 16:14:09 +0530

    JAVASCRIPT LEARNING

---
+++ Javascript/TypeConversion.js
@@ -0,0 +1,60 @@
+let Score = "Ayush"
+
+console.log(Score)
+console.log(typeof(Score))
+
+
+let ValueNumber = Number(Score)
+console.log(typeof(ValueNumber))
+console.log(ValueNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedin = "Ayush"
+let booleanisloggedin = Boolean(isLoggedin)
+console.log(booleanisloggedin)
+
+let onenumber = 21
+console.log(typeof(onenumber))
+let changed_in_string = String(onenumber)
+console.log(changed_in_string)
+console.log(typeof(changed_in_string))
+
+//\\ ++++++++++++++++++++++++  Operation  +++++++++++++++++++ //\\
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Mishra ji"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
