a6ff9a7b45143e352af61211390b68f0a1f36004
Ayush Mishra <officialayushmishra002@gmail.com>
2026-09-08 20:57:35 +0530

    Tailwind use

---
+++ REACT/07-ui-project/src/components/Card.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+
+const Card = () => {
+  return (
+    <div >
+        <div className="flex flex-col-reverse justify-between">
+            <h3>Work Fast. Live Slow</h3>
+        </div>
+
+    </div>
+  )
+}
+
+export default Card
